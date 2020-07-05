af8b2df6e8fe6b6ca0b14decd19150fb8eec6fb2
Sefendy <sefendy@development>
2020-07-05 14:25:36 +0700

    Routing on login and logout

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
+  authSubscription: Subscription;
+  isAuth = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
   onClose() {
