8b36d9a0d19b8e7134e5c0acd815af357f2774af
Sefendy <sefendy@development>
2020-07-09 00:47:40 +0700

    Training services setting ongoing training

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -12,6 +12,7 @@ export class TrainingService {
   ];
 
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
   getAvailableExercises() {
     return this.availableExercises.slice();
@@ -20,5 +21,29 @@ export class TrainingService {
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
     this.exerciseChanged.next({...this.runningExercise});
+  }
+
+  completeExercise() {
+    this.exercises.push({
+      ...this.runningExercise,
+      date: new Date(),
+      state: 'completed'});
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+  cancelExercise(progress: number) {
+    this.exercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100),
+      calories: this.runningExercise.duration * (progress / 100),
+      date: new Date(),
+      state: 'cancelled'
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
   }
 }
