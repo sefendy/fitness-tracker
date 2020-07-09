935016a9782259a1ebf179180ea485bd16de818f
Sefendy <sefendy@development>
2020-07-09 23:41:19 +0700

    Past exercises component setup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -13,7 +13,8 @@ import { MatButtonModule,
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule} from '@angular/material';
+        MatDialogModule,
+        MatTableModule} from '@angular/material';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { MatButtonModule,
     MatCardModule,
     MatSelectModule,
     MatProgressSpinnerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule
     ],
   exports: [
     MatButtonModule,
@@ -48,7 +50,8 @@ import { MatButtonModule,
     MatCardModule,
     MatSelectModule,
     MatProgressSpinnerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule
   ]
 })
 export class MaterialModule {}
